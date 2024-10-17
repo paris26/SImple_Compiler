1991d7c9e22c5fbc883c58fa9cdebe337ca474ff
Paris26 <paris.gvr@gmail.com>
2024-10-17 13:34:56 +0300

    completes lab02 with modified script and insert.txt file

--- flex_compile_run.sh
+++ flex_compile_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Check if a filename was provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <flex_file.l>"
+# Check if both flex file and input file were provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <flex_file.l> <input_file.txt>"
     exit 1
 fi
 
@@ -19,21 +19,40 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Compile the C code
+# Compile the C code with a simple main function
 echo "Compiling..."
-gcc lex.yy.c -lfl -o "$filename"
+cat > temp_main.c << EOL
+#include <stdio.h>
+extern FILE* yyin;
+extern int yylex(void);
+
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        FILE *file = fopen(argv[1], "r");
+        if (!file) {
+            fprintf(stderr, "Could not open file %s\n", argv[1]);
+            return 1;
+        }
+        yyin = file;
+    }
+    yylex();
+    return 0;
+}
+EOL
+
+gcc lex.yy.c temp_main.c -lfl -o "$filename"
 
 if [ $? -ne 0 ]; then
     echo "Compilation failed. Exiting."
     exit 1
 fi
 
-# Run the executable
-echo "Running the program..."
-./"$filename"
+# Run the executable with input file
+echo "Running the program with input file..."
+./"$filename" "$2"
 
 # Clean up
 echo "Cleaning up..."
-rm lex.yy.c "$filename"
+rm lex.yy.c "$filename" temp_main.c
 
 echo "Done."
